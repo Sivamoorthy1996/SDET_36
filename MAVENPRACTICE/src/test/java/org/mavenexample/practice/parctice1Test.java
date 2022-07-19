1521a3adf119146e7af6db696c0c65d1217b9d62
Dinesh.M <Dinesh.M@LAPTOP-8CCMCL84>
2022-07-19 11:44:00 +0530

    1st commit

---
+++ MAVENPRACTICE/src/test/java/org/mavenexample/practice/parctice1Test.java
@@ -0,0 +1,31 @@
+package org.mavenexample.practice;
+
+import org.testng.annotations.Test;
+
+public class parctice1Test {
+	
+	@Test
+	public void parctice1() {
+		String URL = System.getProperty("url");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD= System.getProperty("password");
+		System.out.println("Test1-->class1");
+		System.out.println("url-->"+URL);
+		System.out.println("username-->"+USERNAME);
+		System.out.println("password-->"+PASSWORD);
+	
+	}
+	@Test
+	public void parctice2() {
+		System.out.println("Test1-->class1");
+	
+	}
+	@Test
+	public void parctice3() {
+		System.out.println("Test1-->class1");
+	
+	}
+	
+	
+
+}
