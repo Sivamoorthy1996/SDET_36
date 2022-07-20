2b8f1ff3fbee9ee32f9d4ae04c7244838c627bb4
Dinesh.M <Dinesh.M@LAPTOP-8CCMCL84>
2022-07-20 13:29:32 +0530

    commit today 12

--- MAVENPRACTICE/src/test/java/org/mavenexample/practice/parctice1Test.java
+++ MAVENPRACTICE/src/test/java/org/mavenexample/practice/parctice1Test.java
@@ -22,7 +22,7 @@ public class parctice1Test {
 		System.out.println("Test1-->class1");
 		System.out.println("Good boy");
 		System.out.println("Good boy siva");
-		System.out.println("bad boy siva");
+		System.out.println("bad boy siva and arun");
 	
 	}
 	@Test
